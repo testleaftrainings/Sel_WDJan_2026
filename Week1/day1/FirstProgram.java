0aee790da97a6ff33f89d097d89e39d09188e20f
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-01-05 14:32:37 +0530

    Add files via upload

---
+++ Week1/day1/FirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class FirstProgram {
+	/*
+	 * content assist ---> ctrl+space
+	 * main is entery point to execute a java application
+	 * jvm will execution only after the presence of main method
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to TestLeaf");
+	}
+}
