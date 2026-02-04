6318e0eb9de993259037ea6eab810d956b5e645d
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-02-04 14:58:58 +0530

    Add files via upload

---
+++ Week5/day3/CreateLead.java
@@ -0,0 +1,25 @@
+
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+
+public class CreateLead extends ProjectSpecificMethods {
+	
+	@Test
+	public  void runCreateLead() {
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Testleaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("bhuvanesh");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("moorthy");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("98000");
+		driver.findElement(By.name("submitButton")).click();
+		String text = driver.findElement(By.id("viewLead_companyName_sp")).getText();
+		String[] split = text.split(" ");
+		String actualText =split[0]; 
+		Assert.assertEquals(actualText,"Testleaf");
+	}
+	
+}
